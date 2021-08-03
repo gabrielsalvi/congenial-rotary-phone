84e87f6ac50849373a299b0a937806b16677abc0
gabrielsalvi <gabriel.z.salvi@gmail.com>
2021-08-03 19:04:15 -0300

    adding package arithmeticFundamentals and its 5 files

---
+++ src/main/java/basic/arithmeticFundamentals/Banknotes.java
@@ -0,0 +1,40 @@
+/* Contagem de Cédulas
+
+Faça a leitura de um valor inteiro. Em seguida, calcule o menor número de
+notas possíveis (cédulas) onde o valor pode ser decomposto. As notas que você
+deve considerar são de 100, 50, 20, 10, 5, 2 e 1. Na sequência mostre o valor
+lido e a relação de notas necessárias.
+
+- Entrada: Você receberá um valor inteiro N (0 < N < 1000000).
+
+- Saída: Exiba o valor lido e a quantidade mínima de notas de cada tipo necessárias,
+seguindo o exemplo de saída abaixo. Após cada linha deve ser imprimido o fim
+de linha. */
+
+
+package basic.arithmeticFundamentals;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Banknotes
+{
+    public static void main(String[] args) throws IOException
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        int banknote, i, money;
+        int[] banknotes = {100, 50, 20, 10, 5, 2, 1};
+
+        money = scanner.nextInt();
+
+        System.out.printf("%d\n", money);
+
+        for (i = 0; i < 7; i++)
+        {
+            banknote = money / banknotes[i];
+            money = money % banknotes[i];
+            System.out.printf("%d nota(s) de R$ %d,00\n", banknote, banknotes[i]);
+        }
+    }
+}
---
+++ src/main/java/basic/arithmeticFundamentals/EvenNumbers.java
@@ -0,0 +1,32 @@
+/* Exibindo Números Pares
+
+Crie um programa que leia um número e mostre os números pares até esse
+número, inclusive ele mesmo.
+
+- Entrada: Você receberá 1 valor inteiro N, onde N > 0.
+
+- Saída: Exiba todos os números pares até o valor de entrada, sendo um em cada linha. */
+
+package basic.arithmeticFundamentals;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class EvenNumbers
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int number = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= number; i++)
+        {
+            if (i % 2 == 0)
+            {
+                System.out.println(i);
+            }
+        }
+    }
+}
