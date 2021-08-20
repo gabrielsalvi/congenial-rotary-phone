6e7fb36b707b58215375499999ccedf8da4ebc35
Gabriel Z. Salvi <gabriel.z.salvi@gmail.com>
2021-08-20 10:40:37 -0300

    testing try() with an argument

--- src/main/java/advanced/javaTen/TypeInference.java
+++ src/main/java/advanced/javaTen/TypeInference.java
@@ -1,23 +1,33 @@
 package advanced.javaTen;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.stream.Collectors;
 
 public class TypeInference
 {
-    public static void main(String[] args) throws IOException
+    public static void main(String[] args)
     {
-        var url = new URL("https://pt.wikipedia.org/wiki/Mangak%C3%A1");
-        var urlConnection = url.openConnection();
-
-        var bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-
-        System.out.println(bufferedReader.lines().collect(Collectors.joining()).replaceAll(">", ">\n"));
-
+        printURLAfterConnect();
         printSum(2, 3, 12, 1);
+    }
+
+    private static void printURLAfterConnect()
+    {
+        try
+        {
+            var url = new URL("https://pt.wikipedia.org/wiki/Mangak%C3%A1");
+            var urlConnection = url.openConnection();
+            
+            try(var bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream())))
+            {
+                System.out.println(bufferedReader.lines().collect(Collectors.joining()).replaceAll(">", ">\n"));
+            }
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void printSum(int ... numbers)
