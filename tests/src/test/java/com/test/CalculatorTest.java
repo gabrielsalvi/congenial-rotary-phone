0328ffadf38b89c01ced76302f68b328d5c8745c
Gabriel Z. Salvi <gabriel.z.salvi@gmail.com>
2021-09-14 09:33:35 -0300

    implemented test using mockito

--- tests/src/test/java/com/test/CalculatorTest.java
+++ tests/src/test/java/com/test/CalculatorTest.java
@@ -2,6 +2,8 @@ package com.test;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class CalculatorTest
 {
@@ -11,5 +13,18 @@ public class CalculatorTest
         Calculator calculator = new Calculator();
         int sum = calculator.sum("1+1+3");
         assertEquals(5, sum);
+    }
+
+    @Test
+    public void testeSumWithMock()
+    {
+        String calc = "1+4+7";
+        Calculator calculator = mock(Calculator.class);
+
+        when(calculator.sum(calc)).thenReturn(12);
+
+        int result = calculator.sum(calc);
+
+        assertEquals(12, result);
     }
 }
